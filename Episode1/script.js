b4a57c028da311b72889b34471211331b440740d
Neha Irlekar <nehairlekar@gmail.com>
2023-09-19 20:18:19 +0200

    First commit of react project

---
+++ Episode1/script.js
@@ -0,0 +1,31 @@
+/*
+<div id="parent">
+    <div id="child">
+        <h1>I am h1 tag</h1>
+    </div>
+</div>
+*/
+let parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+]);
+
+/*let heading = React.createElement(
+  "h1",
+  { id: "header" },
+  "Hello from React World"
+);*/
+
+//heading is object
+console.dir(parent);
+
+let root = ReactDOM.createRoot(document.getElementById("root"));
+
+//render is creating object to tag and attaching to root
+root.render(parent);
