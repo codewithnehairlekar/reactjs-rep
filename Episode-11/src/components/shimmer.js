1b4f6633edf992e5bf3b1f4c712bb0ea0cea72c8
Neha Irlekar <nehairlekar@gmail.com>
2023-12-30 19:19:14 +0100

    Styles using tailwind

---
+++ Episode-11/src/components/shimmer.js
@@ -0,0 +1,20 @@
+const Shimmer = () => {
+  return (
+    <div>
+      <button>Top-Restaurants</button>
+      <div className="shimmer-container">
+        <div className="shimmerCards"></div>
+        <div className="shimmerCards"></div>
+        <div className="shimmerCards"></div>
+        <div className="shimmerCards"></div>
+        <div className="shimmerCards"></div>
+        <div className="shimmerCards"></div>
+        <div className="shimmerCards"></div>
+        <div className="shimmerCards"></div>
+        <div className="shimmerCards"></div>
+        <div className="shimmerCards"></div>
+      </div>
+    </div>
+  );
+};
+export default Shimmer;
